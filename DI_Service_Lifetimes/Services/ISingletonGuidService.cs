300db2527fe1ade50bda114c5970962489e8807e
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2024-12-18 14:11:56 +0530

    Add project files.

---
+++ DI_Service_Lifetimes/Services/ISingletonGuidService.cs
@@ -0,0 +1,7 @@
+﻿namespace DI_Service_Lifetimes.Services
+{
+    public interface ISingletonGuidService
+    {
+        string GetGuid();
+    }
+}
---
+++ DI_Service_Lifetimes/Services/ITransientGuidService.cs
@@ -0,0 +1,7 @@
+﻿namespace DI_Service_Lifetimes.Services
+{
+    public interface ITransientGuidService
+    {
+        string GetGuid();
+    }
+}
